391d12d19b86b828de1dd297d60cad59c3a51f8f
Sean Leong (WMDE) <sean.leong@wikimedia.de>
2026-03-04 09:51:19 +0100

    Migration of 'From Wikidata' tracker from eventLog to using TestKitchen
    Bug: T417510
    Bug: T415210
    Change-Id: I8bb09c248e6d69af60b289b5b31ea2bdd5c051e3

--- client/resources/wikibase.databox.fromWikidata.js
+++ client/resources/wikibase.databox.fromWikidata.js
@@ -1,19 +1,44 @@
 'use strict';
 /**
- * Add tracking to the "From Wikidata" links in Databox
- * note that the class databox-from-wikidata-link in the module is only used for this tracking so could be removed after
+ * Add code for analytic instruments to the "From Wikidata" links in Databox
+ * note that the class databox-from-wikidata-link in the module is only
+ * used for this tracking so could be removed after
  *
- * Temporary: We will remove it by Feb. 2026
  * Bug: T408709
  *
  * @license GPL-2.0-or-later
  */
-// EventLogging may not be installed
-mw.loader.using( 'ext.eventLogging' ).then( () => {
-	$( '.databox-from-wikidata-link' ).on( 'click', 'a.extiw', () => {
-		mw.eventLog.submitClick( 'wikibase.client.interaction', {
-			// eslint-disable-next-line camelcase
-			action_source: 'wbFromWikidataClick'
-		} );
-	} );
-} );
+(
+	function () {
+		/**
+		 * @param {jQuery} $content
+		 * @param {mw.testKitchen.InstrumentInterface} instrument
+		 */
+		function addFromWikidataTracking( $content, instrument ) {
+			const $fromWikidataLink = $content.find( '.databox-from-wikidata-link' );
+
+			$fromWikidataLink.on( 'click', () => {
+				instrument.send( 'click', {
+					// eslint-disable-next-line camelcase
+					action_source: 'wbFromWikidataClick'
+				} );
+			} );
+		}
+
+		/**
+		 * Adds temporary tracking for user interactions
+		 *
+		 * @param {jQuery} $content
+		 */
+		function addFromWikidataInstrument( $content ) {
+			/** @type {mw.testKitchen.InstrumentInterface|undefined} */
+			const fromWikidataLinkInstrument = mw.testKitchen && mw.testKitchen.getInstrument( 'databox-click-tracker' );
+
+			if ( fromWikidataLinkInstrument && fromWikidataLinkInstrument.isInSample() ) {
+				addFromWikidataTracking( $content, fromWikidataLinkInstrument );
+			}
+		}
+
+		mw.hook( 'wikipage.content' ).add( addFromWikidataInstrument );
+	}()
+);
